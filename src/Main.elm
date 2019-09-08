7b38d94f9f591c61feb9e59eae3facc87eb56dc5
siddharth <siddhartjn93@gmail.com>
2019-09-08 16:05:52 +0530

    "css set"

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,42 @@
-module Main exposing (main)
-
 import Browser
-import Html exposing (h1, text)
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+
 
 main =
-  h1 [] [ text "Hello World hot reloading!ffffv" ]
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+-- MODEL
+
+type alias Model = Int
+
+init : Model
+init =
+  0
+
+
+-- UPDATE
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div [class "pa2 b-red"]
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
